909f9c5ff7f70915e3be4125270027b892fbcba0
Shady Emad <95425147+ShaadyEmad@users.noreply.github.com>
2022-06-24 17:11:35 +0200

    Add files via upload

---
+++ src/password_generator/Password_Generator.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package password_generator;
+
+/**
+ *
+ * @author Shady Emad
+ */
+public class Password_Generator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       new Generator().setVisible(true);
+    }
+    
+}
